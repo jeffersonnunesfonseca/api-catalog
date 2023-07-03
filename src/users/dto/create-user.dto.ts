108e2a95637f1875659b3f908f6990875398c4e5
jeffersonnunesfonseca <jeffersonnunesfonseca@gmail.com>
2023-07-02 22:34:03 -0300

    first

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import {
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+import { Role } from '@Enums/role.enum';
+
+export class CreateUserDto {}
